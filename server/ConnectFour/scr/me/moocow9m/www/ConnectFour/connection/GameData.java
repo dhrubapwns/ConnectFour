360e872ae8106f42e84ae6a7b2582a08919c4998
Moocow9m <devmoocow9m@gmail.com>
2016-03-25 13:27:33 -0700

    Create GameData.java

---
+++ server/ConnectFour/scr/me/moocow9m/www/ConnectFour/connection/GameData.java
@@ -0,0 +1,11 @@
+package me.moocow9m.www.ConnectFour.connection;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by Moocow9m on 3/25/2016.
+ */
+public class GameData implements Serializable {
+
+}
